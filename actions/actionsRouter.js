23ef3b303658fb19e50eb2258e76c6c119c1346c
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-06 09:33:20 -0800

    Fix middleware and post utilize correct persistent helpers. Create PUT and DELETE for actions.

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -35,19 +35,43 @@ router.post('/', validateId, (req, res) => {
         })
 });
 
+router.delete('/:id', (req, res) => {
+    const id = req.params.insert
+    actModel.remove(id)
+        .then(deletedAction => {
+            res.status(200).json({ message: `The action with id: ${id} was deleted`, deletedAction })
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "There was an error deleting the action." })
+        })
+
+});
+
+router.put('/:id', validateId, (req, res) => {
+    const id = req.params.id
+    const creAct = req.body
+    if (!creAct.description || !creAct.notes) {
+        res.status(400).json({ message: "Please provide update" })
+    } else {
+        actModel.update(id, creAct)
+            .then(updateAction => {
+                res.status(200).json({ message: "Updated with", action: `${creAct.description} ${creAct.notes}` })
+            })
+            .catch((error) => {
+                res.status(500).json({ message: "There was an error with the update.", error })
+            })
+    }
+
+
+});
+
 // custom middleware
 function validateId(req, res, next) {
     const id = req.params.id
     if (id >= 3) {
         res.status(404).json({ errorMessage: "There is no id with that in the database" })
     } else {
-        actModel.getById(id)
-            .then(found => {
-                res.status(200).json(found)
-            })
-            .catch(() => {
-                res.status(500).json({ message: "There was an error with that id." })
-            })
+        res.status(200).json({ message: "The id is good" })
     }
     next();
 }
